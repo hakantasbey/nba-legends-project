014fefce407066c2dd8d16b0804768132d484f6d
hakan <tasbeyhakan@gmail.com>
2023-12-05 23:10:03 +0300

    end of the project

--- nba-legends-project/src/components/CardContainer.jsx
+++ nba-legends-project/src/components/CardContainer.jsx
@@ -26,7 +26,7 @@ const CardContainer = () => {
             key={name}
             name={name}
             img={img}
-            istatistics={statistics}
+            statistics={statistics}
             />
         ))}
         </div>
