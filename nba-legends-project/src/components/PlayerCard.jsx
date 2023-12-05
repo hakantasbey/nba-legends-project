fd39cbdc5ec5f114df30e4809ca16edcfeed7015
hakan <tasbeyhakan@gmail.com>
2023-12-05 23:00:29 +0300

    playerCard commit

--- nba-legends-project/src/components/PlayerCard.jsx
+++ nba-legends-project/src/components/PlayerCard.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PlayerCard = ({name,img,statistics}) => {
+  const [displayStats, setDisplayStats] = useState(false)
+  // const handleDisplay = () =>{
+  //   setDisplayStats(!displayStats)
+  // }
   return (
     <div>
-      <img src={img} alt="" width="200px"/>
-      <h4>{name}</h4>
+    <div className='card' onClick={()=>setDisplayStats(!displayStats)}>
+      {displayStats ? (
+          <ul>
+            {statistics.map((stats,index)=>(
+              <li key={index}>{stats}</li>
+            ))}
+          </ul>
+      ) : (
+          <div>
+            <img src={img} alt={name} width="300px" />
+          </div>
+      )}
+    </div>
+    <div>
+        <a target='_blank' href={`https://en.wikipedia.org/wiki/${name}`}>{name}</a>
+      </div>
     </div>
   )
 }
